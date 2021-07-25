724fc2642e11637564b2cfd5dd9edff4f357a00e
Swetha <swethasivadas.95@gmail.com>
2021-07-25 21:53:17 +0530

    added methods

--- ifsc_multiple.rb
+++ ifsc_multiple.rb
@@ -1,27 +1,36 @@
 require 'net/http'
 require 'json'
 
-url = 'https://ifsc.razorpay.com/'
+API_URL = 'https://ifsc.razorpay.com/'
+
+def make_request(ifsc_code)
+  ifsc_link = API_URL + ifsc_code
+  uri = URI(ifsc_link)
+  Net::HTTP.get_response(uri)
+end
+
+def print_details(ifsc_code)
+  response = make_request(ifsc_code)
+
+  if response.code.to_i == 404
+    puts "Entered IFSC Code : " + ifsc_code + " is INVALID! Please re-enter correct one"
+  else
+    details = JSON.parse(response.body)
+    puts "Details for bank with IFSC code: " + ifsc_code
+    puts "-------------------------------------"
+    puts "Your Bank name is " + details["BANK"]
+    puts "Your Bank Branch name is " + details["BRANCH"]
+    puts "-------------------------------------"
+  end
+end
+
 puts "Please type in your IFSC code(s): (put comma for multiple entires)"
 ifsc_codes = gets.chomp
+
 ifsc_array = ifsc_codes.split(",").uniq
 
 puts "IFSC code(s) reconfirmation : " + ifsc_codes
 
 ifsc_array.each do |ifsc_code|
-	ifsc_link = url + ifsc_code
-	uri = URI(ifsc_link)
-  response = Net::HTTP.get_response(uri)
-  status = response.code
-
-  if status.to_i == 404
-    puts "Entered IFSC Code : " + ifsc_code + " is INVALID! Please re-enter correct one"
-  else
-    details = JSON.parse(response.body)
-    puts "Details for bank with IFSC code: " + ifsc_code
-    puts "-------------------------------------" 
-    puts "Your Bank name is " + details["BANK"]
-    puts "Your Bank Branch name is " + details["BRANCH"]
-    puts "-------------------------------------" 
-  end
+  print_details(ifsc_code)
 end
